2a75b3863b3c7f0871b1a61722b4f1659c6699a4
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-17 14:09:15 -0700

    tried to sort dead end

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,9 +18,9 @@ module.exports = (app) => {
             }
             scoreArr.push(count);
         }
+        scoreArr.sort(function(a, b){return a-b});
         console.log(scoreArr);
 
-        
 
 
 
