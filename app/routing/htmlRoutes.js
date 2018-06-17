bb59c533f51fe45a7696e25564c7972ea72ea025
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-16 20:27:54 -0700

    progress finally

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,13 @@
-app.get('/', (req, res) => {
-    res.send('Hello Homepage');
-});
+const path = require('path');
 
-app.get('/survey', (req, res) => {
-    res.send('You are in the survey page');
-});
+
+module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+        // res.send('This is right');
+    });
+
+    app.get('/survey', (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+}
--- server.js
+++ server.js
@@ -6,15 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
  
 // parse application/json
 app.use(bodyParser.json());
  
-app.use((req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-});
+// require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-app.listen(PORT, () => console.log(`Listening on port ${port}...`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
